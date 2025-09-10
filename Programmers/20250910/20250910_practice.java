94577fedf56f5739dfa73fb226edabab8a2a9251
sjlimiei25 <sjlim.iei.25@gmail.com>
2025-09-10 10:49:34 +0900

    Create 20250910_practice.java

---
+++ Programmers/20250910/20250910_practice.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
